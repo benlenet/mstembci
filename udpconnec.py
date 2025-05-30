50d19b7ee810c30e57b1b78d5b75e0292b562f8c
E203 G.tec User <e203gtec@acad.fullerton.edu>
2025-05-30 14:57:50 -0700

    readjusted udp code

--- udpconnec.py
+++ udpconnec.py
@@ -1,18 +1,24 @@
 import socket
-
+import keyboard
 # marker send to MATLAB
 # # 🔹 UDP Setup
 UDP_IP = "127.0.0.1"
 UDP_PORT = 8000
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
+# sock.bind((UDP_IP, UDP_PORT))
 # 🔹 EEG Stream Settings
 # fs = 500  # Sampling rate (Hz)
 # samples_per_packet = 125  # Send 50 samples per packet (0.1 sec of data)
 
-eeg_bytes = bytearray(b'1')
-sock.sendto(eeg_bytes, (UDP_IP, UDP_PORT))
-
+while(1):
+    userval = input("type number value to send to udp: ")
+    try:
+        eegbytes = bytes([int(userval)])
+    except:
+        print("cannot cast", userval, "as int\n")
+        continue
+    sock.sendto(eegbytes, (UDP_IP, UDP_PORT))
+    print("sent to udp value:", eegbytes, '\n')
 
 # retrieve frame of PsychoPy  
 def timesync():
